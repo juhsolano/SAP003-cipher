7da29955faf83ca87f8b63b26b4c983e205a118e
juhsaolno <juanita.hsolano@gmail.com>
2019-08-23 16:26:19 -0300

    Primeira edicao das funçoes encode/decode e esboço html

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,32 @@
 window.cipher = {
-  // ... 
+  encode: encode,
+  decode: decode,
 };
+
+function encode (offset, str) {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
+      result += String.fromCharCode((str.charCodeAt(i) - 65 + offset) % 26 + 65);
+    } else if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122) {
+      result += String.fromCharCode((str.charCodeAt(i) - 97 + offset) % 26 + 97);
+    } else {
+      result += str[i];
+    }  
+  }
+  return result;
+}
+
+function decode (offset, str) {
+  let decoResult = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
+      decoResult += String.fromCharCode((str.charCodeAt(i) - 90 - offset) % 26 +90);
+    } else if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122) {
+      decoResult += String.fromCharCode((str.charCodeAt(i) -122 - offset) % 26 +122);
+    } else {
+      decoResult += str[i];
+    }
+  }
+  return decoResult;
+}
---
+++ src/index.js
@@ -0,0 +1,16 @@
+function btnEncode() {
+  event.preventDefault();
+  let offset = parseInt(document.getElementById("eKey").value);
+  let str = (document.getElementById("eMsg").value);
+  let outCode = window.cipher.encode(offset, str);
+  document.getElementById("outCode").innerHTML = `<p> Sua mensagem: ${outCode}</p>`;
+}
+btnEncode();
+function btnDecode() {
+  event.preventDefault();
+  let offset = parseInt(document.getElementById("dKey").value);
+  let str = (document.getElementById("dMsg").value);
+  let outDecode = window.cipher.decode(offset, str);
+  document.getElementById("outDecode").innerHTML = `<p> O que elas dizem: ${outDecode}</p>`;
+}
+btnDecode();
