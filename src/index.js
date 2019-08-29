9ec441ba17ad9224bf5e6aa4275d7a728267efad
juhsaolno <juanita.hsolano@gmail.com>
2019-08-29 16:51:48 -0300

    reestruturacao no HTML e edicao do CSS(estrutura do site).

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ function btnEncode() {
   let outCode = window.cipher.encode(offset, str);
   document.getElementById("outCode").innerHTML = `<p> Sua mensagem: ${outCode}</p>`;
 }
-btnEncode();
+document.getElementById("send").addEventListener("click", btnEncode);
+
 function btnDecode() {
   event.preventDefault();
   let offset = parseInt(document.getElementById("dKey").value);
@@ -13,4 +14,4 @@ function btnDecode() {
   let outDecode = window.cipher.decode(offset, str);
   document.getElementById("outDecode").innerHTML = `<p> O que elas dizem: ${outDecode}</p>`;
 }
-btnDecode();
+document.getElementById("send-emsg").addEventListener("click", btnDecode);
